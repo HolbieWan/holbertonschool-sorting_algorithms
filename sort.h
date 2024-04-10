175a81360c887b7dee2cc3eb5793b393a16780a5
HolbieWan <8841@holbertonstudents.com>
2024-04-10 07:57:57 +0000

    Added files: print_array.c, print_list.c

--- sort.h
+++ sort.h
@@ -16,5 +16,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif /*__SORT_H__*/
