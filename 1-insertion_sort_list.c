98a1e6e86beda41570e3ada8c1849f9957481cb8
HolbieWan <8841@holbertonstudents.com>
2024-04-10 12:36:08 +0000

    Updated files: print_list.c, print_array.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -19,15 +19,45 @@ void swap_array_i(int *array, int i, int j)
 }
 
 /**
- * bubble_sort - function that sorts an array of integers
- * in ascending order using the Bubble sort algorithm
+ * insertion_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Insertion sort algorithm
  *
- * @array: the array of int to sort
- * @size: the size of the given array
+ * @list: the double linked list
  *
  * Return: Void
  */
-void bubble_sort(int *array, size_t size)
+void insertion_sort_list(listint_t **list)
 {
-	
+	{
+    // zero or one element in list
+    if (pList == NULL || pList->pNext == NULL)
+        return pList;
+    // head is the first element of resulting sorted list
+    struct LIST * head = NULL;
+    while (pList != NULL) {
+        struct LIST * current = pList;
+        pList = pList->pNext;
+        if (head == NULL || current->iValue < head->iValue) {
+            // insert into the head of the sorted list
+            // or as the first element into an empty sorted list
+            current->pNext = head;
+            head = current;
+        } else {
+            // insert current element into proper position in non-empty sorted list
+            struct LIST * p = head;
+            while (p != NULL) {
+                if (p->pNext == NULL || // last element of the sorted list
+                    current->iValue < p->pNext->iValue) // middle of the list
+                {
+                    // insert into middle of the sorted list or as the last element
+                    current->pNext = p->pNext;
+                    p->pNext = current;
+                    break; // done
+                }
+                p = p->pNext;
+            }
+        }
+    }
+    return head;
+}
 }
