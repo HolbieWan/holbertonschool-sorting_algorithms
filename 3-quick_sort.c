1d9fd6210e9ffd1455f38c1de6ccbc03c49d39ed
HolbieWan <8841@holbertonstudents.com>
2024-04-11 18:49:37 +0000

    Updated file: 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,5 +9,33 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	
+	int i = 0, j;
+	int pivot = size - 1;
+	int elem_to_swap;
+	int temp;
+
+	while (array[i] != '\0')
+	{
+		if (array[i] > array[pivot])
+			elem_to_swap = i;
+
+		for (j = i + 1; j <= pivot; j++)
+		{
+			if (array[j] <= array[pivot] && array[pivot] < array[i])
+			{
+				temp = array[j];
+				array[j] = array[elem_to_swap];
+				array[elem_to_swap] = temp;
+				print_array(array, size);
+			}
+			else if (array[j] < array[pivot] && array[j] > array[j+1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
