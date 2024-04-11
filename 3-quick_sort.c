4f7b821a848ed46052a82b2c8ce79d79fd131cbd
HolbieWan <8841@holbertonstudents.com>
2024-04-11 21:15:44 +0000

    Updated file: 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+
+
 /**
 * quick_sort - function that sorts an array of integers in ascending order
 *				 using the Quick sort algorithm and the Lomuto partition scheme
@@ -38,4 +40,30 @@ void quick_sort(int *array, size_t size)
 		}
 		i++;
 	}
+	i = 0;
+	while (array[i] != '\0')
+	{
+		if (array[i] > array[pivot])
+			elem_to_swap = i;
+
+		for (j = i + 1; j <= pivot; j++)
+		{
+			if (array[j] <= array[pivot] && array[pivot] < array[i])
+			{
+				temp = array[j];
+				array[j] = array[elem_to_swap];
+				array[elem_to_swap] = temp;
+				print_array(array, size);
+			}
+			else if (array[j] < array[pivot] && array[j] > array[j+1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
+
