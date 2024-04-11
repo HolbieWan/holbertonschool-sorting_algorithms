87a792cf4e53868fd1c9f5c5f511bdba51a620a9
niranois13 <8784@holbertonstudents.com>
2024-04-11 02:12:00 +0200

    correcting selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,20 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j]  < array[i])
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j]  < array[i])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
