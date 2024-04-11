548ffe66fabe0994f17fe4ac087aea7da5708550
niranois13 <8784@holbertonstudents.com>
2024-04-11 14:00:31 +0200

    creating files for quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* quick_sort - function that sorts an array of integers in ascending order
+*				 using the Quick sort algorithm and the Lomuto partition scheme
+* @array: pointer to the array to sort
+* @size: size of the array
+* Return: void
+*/
+void quick_sort(int *array, size_t size)
+{
+	
+}
